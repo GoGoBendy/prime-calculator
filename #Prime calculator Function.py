937df1944d9952adf77df6a4d2f75a87bb28f531
GoGoBendy <110510326+GoGoBendy@users.noreply.github.com>
2023-08-08 08:57:39 +1000

    bug

--- #Prime calculator Function.py
+++ #Prime calculator Function.py
@@ -1,8 +1,8 @@
 #Prime calculator Function
 def prime_calculator(number):
-    divisable = 2
+    divisable = int(2)
     for divisable in number:
-        if number % divisable:
+        if number / divisable:
             print(divisable)
             divisable + 1
         else:
@@ -10,6 +10,9 @@ def prime_calculator(number):
     
 
 #collecting a number from user
-num = input(print("Please input a number for testing"))
+print("please input number for testing")
+num = int(input())
 #calling the function
 prime_calculator(num)
+end = input()
+print("end")
