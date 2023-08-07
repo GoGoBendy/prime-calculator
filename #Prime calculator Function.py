b66830e472d5255bf0106fdb649002275d874810
GoGoBendy <110510326+GoGoBendy@users.noreply.github.com>
2023-08-07 13:39:06 +1000

    start

---
+++ #Prime calculator Function.py
@@ -0,0 +1,10 @@
+#Prime calculator Function
+def prime_calculator(number):
+    int(divisable) = 2
+    for divisable =/ number:
+        if number % divisable == 0:
+        divisable + 1
+        
+    else:
+        divisable + 1
+    
