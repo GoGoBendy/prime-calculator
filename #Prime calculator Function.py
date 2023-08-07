7a686de6b35feb0e2ed196226312157822c817d6
GoGoBendy <110510326+GoGoBendy@users.noreply.github.com>
2023-08-08 08:24:38 +1000

    fixing syntax

--- #Prime calculator Function.py
+++ #Prime calculator Function.py
@@ -1,10 +1,15 @@
 #Prime calculator Function
 def prime_calculator(number):
-    int(divisable) = 2
-    for divisable =/ number:
-        if number % divisable == 0:
-        divisable + 1
-        
-    else:
-        divisable + 1
+    divisable = 2
+    for divisable in number:
+        if number % divisable:
+            print(divisable)
+            divisable + 1
+        else:
+            divisable + 1
     
+
+#collecting a number from user
+num = input(print("Please input a number for testing"))
+#calling the function
+prime_calculator(num)
