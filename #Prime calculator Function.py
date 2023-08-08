590ebc123185826796aa376a6a539c53dc5f13f6
GoGoBendy <110510326+GoGoBendy@users.noreply.github.com>
2023-08-08 11:59:36 +1000

    attempt at fixing bug, made another one

--- #Prime calculator Function.py
+++ #Prime calculator Function.py
@@ -1,12 +1,14 @@
 
 #collecting a number from user
 print("please input number for testing")
-num = int(input())
+num = input()
+num = int(num)
 #calling the function
 divisable = int(2)
-for divisable in num:
-    if num / divisable:
+for divisable in range(num):
+    if num / divisable == 0:
         print(divisable)
         divisable + 1
     else:
+        print(f"So far good {divisable}")
         divisable + 1
