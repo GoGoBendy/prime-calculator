dfaab5848eb17c520212ffc72febe490501445e0
GoGoBendy <110510326+GoGoBendy@users.noreply.github.com>
2024-03-21 10:43:29 +1100

    added generation of a select number of primes

---
+++ prime_number_generator_deluxe.py
@@ -0,0 +1,40 @@
+"""Module providing a function generating prime numbers from a list."""
+
+
+# function to return prime numbers from list
+def prime(numlis):
+    """function verifying if a number is prime"""
+    numberlist = []
+    numb = int(numlis)
+    div = True
+    for divisable in range(2, numb):
+        if numb % divisable == 0:
+            divisable += 1
+            div = False
+        else:
+            divisable += 1
+    if div is True:
+        nume = str(numlis)
+        numberlist.append(nume)
+    numberliststr = " ".join(numberlist)
+    return numberliststr
+
+
+def generator(number):
+    """function to send generated numbers for verification"""
+    nums = 0
+    yesno = 0
+    numlist = []
+    while nums != number:
+        nono = prime(yesno)
+        if nono != "":
+            numlist.append(nono)
+            nums += 1
+        yesno += 1
+    return ", ".join(numlist)
+
+
+# collecting a number from user
+num = int(input("how many primes would you like to generate?? "))
+# calling the function
+print(generator(num))
